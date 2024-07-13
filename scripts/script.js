c530ad7f6ad1e9c165962583ff8113af6ae907a1
gcao <guanyi.cao@princeton.edu>
2024-07-13 19:38:16 -0400

    test

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,5 @@
 document.addEventListener('mousemove', (event) => {
     const blob = document.querySelector('.blob');
-    const mouseX = event.pageX;
-    const mouseY = event.pageY;
-
-    blob.style.left = mouseX + 'px';
-    blob.style.top = mouseY + 'px';
+    blob.style.left = event.pageX + 'px';
+    blob.style.top = event.pageY + 'px';
 });

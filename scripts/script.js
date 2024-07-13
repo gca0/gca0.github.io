d6ee6fade2a260006c9d1d50145976c06e91637d
gcao <guanyi.cao@princeton.edu>
2024-07-13 19:21:58 -0400

    added background effect

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
-
+document.addEventListener('mousemove', (event) => {
+    const mouseX = event.pageX;
+    const mouseY = event.pageY;
+    
+    const light = document.createElement('div');
+    light.className = 'light';
+    light.style.left = mouseX + 'px';
+    light.style.top = mouseY + 'px';
+    
+    document.body.appendChild(light);
+    
+    setTimeout(() => {
+        light.remove();
+    }, 2000); // Remove the light element after 2 seconds
+});

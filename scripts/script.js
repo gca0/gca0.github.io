50bd787cf8996e1c0a36e5bc1afd195cf565ba9d
gcao <guanyi.cao@princeton.edu>
2024-07-13 21:09:07 -0400

    cursor animation

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,49 @@
-document.addEventListener('mousemove', (event) => {
-    const trailContainer = document.getElementById('trail');
-    const trailDot = document.createElement('div');
-    trailDot.classList.add('trail-dot');
+// document.addEventListener('mousemove', (event) => {
+//     const trailContainer = document.getElementById('trail');
+//     const trailDot = document.createElement('div');
+//     trailDot.classList.add('trail-dot');
     
-    const color = generateRandomColor(); // Function to generate random color gradient
-    trailDot.style.backgroundColor = color;
+//     const color = generateRandomColor(); // Function to generate random color gradient
+//     trailDot.style.backgroundColor = color;
     
-    trailDot.style.left = event.pageX + 'px';
-    trailDot.style.top = event.pageY + 'px';
+//     trailDot.style.left = event.pageX + 'px';
+//     trailDot.style.top = event.pageY + 'px';
     
-    trailContainer.appendChild(trailDot);
+//     trailContainer.appendChild(trailDot);
     
-    // Remove dot after animation ends
-    setTimeout(() => {
-        trailDot.remove();
-    }, 1000); // Adjust based on the duration of animation
-});
+//     // Remove dot after animation ends
+//     setTimeout(() => {
+//         trailDot.remove();
+//     }, 1000); // Adjust based on the duration of animation
+// });
 
-function generateRandomColor() {
-    const hue = Math.floor(Math.random() * 360);
-    return `hsl(${hue}, 100%, 50%)`;
-}
+// function generateRandomColor() {
+//     const hue = Math.floor(Math.random() * 360);
+//     return `hsl(${hue}, 100%, 50%)`;
+// }
+const cursor = document.querySelector(".cursor");
+var timeout;
+
+// follow cursor on mouse move
+document.addEventListener("mousemove", (e) => {
+    let x = e.pageX;
+    let y = e.pageY;
+
+    cursor.style.top = y + "px";
+    cursor.style.left = x + "px";
+    cursor.style.display = "block";
+
+    // cursor effects on mouse stopped
+    function mouseStopped() {
+        cursor.style.display = "none";
+
+    }
+    clearTimeout(timeout);
+    timeout = setTimeout(mouseStopped, 1000);
+
+} );
+
+// cursor effects on mouse out
+document.addEventListener("mouseout", () => {
+    cursor.style.display = "none";
+})

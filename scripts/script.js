883be0ea346f7342bcb827c209a720139a593c0b
gcao <guanyi.cao@princeton.edu>
2024-07-14 20:42:22 -0400

    new cursor shape

--- scripts/script.js
+++ scripts/script.js
@@ -95,6 +95,35 @@ function deletingEffect() {
 typingEffect();
 
 
+const cursor = document.querySelector(".cursor");
+var timeout;
+
+// follow cursor on mouse move
+document.addEventListener("mousemove", (e) => {
+    let x = e.pageX;
+    let y = e.pageY;
+
+    cursor.style.top = y + "px";
+    cursor.style.left = x + "px";
+    cursor.style.display = "block";
+
+    // // cursor effects on mouse stopped
+    // function mouseStopped() {
+    //     cursor.style.display = "none";
+
+    // }
+    // clearTimeout(timeout);
+    // timeout = setTimeout(mouseStopped, 1000);
+
+});
+
+// cursor effects on mouse out
+document.addEventListener("mouseout", () => {
+    cursor.style.display = "none";
+})
+
+
+
 
 
 

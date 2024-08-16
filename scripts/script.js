7520694f1e05fd2703004c8cae42ae674e64e7e7
gcao <guanyi.cao@princeton.edu>
2024-08-15 21:36:44 -0400

    delete commented out code

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,3 @@
-
-// const cursor = document.querySelector(".cursor");
-// var timeout;
-
-// // follow cursor on mouse move
-// document.addEventListener("mousemove", (e) => {
-//     let x = e.pageX;
-//     let y = e.pageY;
-
-//     cursor.style.top = y + "px";
-//     cursor.style.left = x + "px";
-//     cursor.style.display = "block";
-
-//     // // cursor effects on mouse stopped
-//     // function mouseStopped() {
-//     //     cursor.style.display = "none";
-
-//     // }
-//     // clearTimeout(timeout);
-//     // timeout = setTimeout(mouseStopped, 1000);
-
-// } );
-
-// // cursor effects on mouse out
-// document.addEventListener("mouseout", () => {
-//     cursor.style.display = "none";
-// })
 document.addEventListener('DOMContentLoaded', () => {
     const interBubble = document.querySelector('.interactive');
     let curX = 0;
